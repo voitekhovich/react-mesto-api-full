f7e17a9613989fc0bbf782aa88251eff647c6baf
voitekhovich <a.voitekhovich@gmail.com>
2023-09-17 17:16:44 +0300

    Add react pages

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { cors } = require('./middlewares/cors');
 
-const { PORT = 3000, MONGO_DB_URL = 'mongodb://localhost:27017/mestodb2' } = process.env;
+const { PORT = 3000, MONGO_DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 mongoose.connect(MONGO_DB_URL);
